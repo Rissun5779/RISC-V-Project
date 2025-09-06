43c00d08d91263b29cac93a043b144f509fcbca9
Rissun5779 <rissun57@gmail.com>
2025-09-06 19:37:11 +0800

    re-add build folder with LFS

--- T_core/TCORE-RISCV-FreedomE300/freedom-e-sdk/work/build/riscv-gnu-toolchain/riscv64-unknown-elf/prefix/lib/gcc/riscv64-unknown-elf/8.1.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/home/terasic/Desktop/freedom-e-sdk/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/home/terasic/Desktop/freedom-e-sdk/work/build/riscv-gnu-toolchain/riscv64-unknown-elf/prefix --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/terasic/Desktop/freedom-e-sdk/work/build/riscv-gnu-toolchain/riscv64-unknown-elf/prefix/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --src=/home/terasic/Desktop/freedom-e-sdk/riscv-gnu-toolchain/riscv-gcc --enable-checking=yes --enable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-Os  -mcmodel=medany'";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
